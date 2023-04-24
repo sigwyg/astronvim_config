967e69daef4e33e036d8f0565ba4eb16e9dc4394
sigwyg <sigwyg@users.noreply.github.com>
2023-04-25 00:47:25 +0900

    add trouble and syntax

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
 }
